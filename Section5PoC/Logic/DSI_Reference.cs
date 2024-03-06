e764b5532bd9cb4b76e1fc05b722510c078b2f37
Tom <454147@student.fontys.nl>
2024-03-06 16:31:33 +0100

    added DSI_Reference.cs object Currently DSI_Reference objects are stored in the old tool in a hidden sheet. Adding this functionality would require a database of some sorts!

---
+++ Section5PoC/Logic/DSI_Reference.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section5PoC.Logic
+{
+    public class DSI_Reference
+    {
+        public string YearWeek { get; private set; }
+        public string BundleNumber { get; private set; }
+        public string ProjectName { get; private set; }
+        public string Description { get; private set; }
+        
+        public DSI_Reference(string yearWeek, string bundleNumber, string projectName, string description) 
+        {
+            YearWeek = yearWeek;
+            BundleNumber = bundleNumber;
+            ProjectName = projectName;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
