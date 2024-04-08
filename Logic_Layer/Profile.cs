8b62a87e08ef05c73583e1471d167fd25a6788a5
Tom <454147@student.fontys.nl>
2024-04-08 14:01:17 +0200

    added Profile.cs

---
+++ Logic_Layer/Profile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic_Layer
+{
+    public class Profile
+    {
+        enum ProfileType
+        {
+
+        }
+        public List<string> Parameters;
+        public ProfileType type;
+        public Profile()
+        {
+
+        }
+    }
+}
