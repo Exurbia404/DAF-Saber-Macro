c5cbc0b7007753246297a46f94731a8278978a50
Tom <454147@student.fontys.nl>
2024-05-08 14:27:54 +0200

    Started implementing a class that holds Settings.cs and a class that holds FolderPaths.cs

---
+++ Presentation/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation
+{
+    public static class Settings
+    {
+        public const string ProductionFolder;
+        public const string ReldasFolder;
+        public const string DesignerFolder;
+
+        public const string LocalFolder = FolderPaths.ExurbiaLocal;
+    }
+}
